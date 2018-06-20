22bfc638945578f0f9c5d32bf62ffb8559680004
abadug5793 <bavani7777@gmail.com>
2018-06-20 14:33:56 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,5 @@
-def simple_curfew_checker(time)
-  # code goes here
-end
-
 def curfew_checker(time)
-  # code goes here
-end
-
-def complex_curfew_checker(time)
-  # code goes here
-end
-
-def deluxe_curfew_checker(time)
-  # code goes here
-end
-
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+  if time == 11 && time > 11
+    puts "You're in Trouble"
+  else
+    puts "You're Not in Trouble"
